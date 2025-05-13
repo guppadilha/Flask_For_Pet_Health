2575f470e7e9d9fbe7ac59bcbb73210ca41a4372
guppadilha <116732302+guppadilha@users.noreply.github.com>
2025-05-13 20:07:38 -0300

    fix: correção endpoint protocolo

--- main.py
+++ main.py
@@ -3,7 +3,4 @@ from website import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    # Inicializa o banco de dados (cria as tabelas se não existirem)
-    with app.app_context():
-        db.create_all() # db precisa ser importado de website ou website.models
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True)
