0919f8be410f273d322485a77426cc63626d3ace
Gustavo Padilha <116732302+guppadilha@users.noreply.github.com>
2025-05-13 19:51:17 -0300

    fix: Utiliza o create app corretamente

--- main.py
+++ main.py
@@ -3,4 +3,7 @@ from website import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Inicializa o banco de dados (cria as tabelas se não existirem)
+    with app.app_context():
+        db.create_all() # db precisa ser importado de website ou website.models
+    app.run(debug=True, host='0.0.0.0', port=5000)
